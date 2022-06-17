eb8e2e7e5da6360f5f60212dbe1340208c6e8423
mozn-lz <mozn.lozn2000@gmail.com>
2022-06-17 13:50:19 +0200

    cleanup; search code

--- routes/search.js
+++ routes/search.js
@@ -1,49 +1,21 @@
 const express = require('express');
+const { db_read } = require('./db_helper');
 const router = express.Router();
-const { db_search } = require('./db_helper');
 
-let find_suppliers = (str) => {
+let find_users = (str) => {
 	return new Promise((resolve, reject) => {
-		db_search('suppliers', 'supplier_name', str, suppliers => {
-			console.log('1', suppliers.length);
-			resolve(suppliers);
-		});
-	});
-}
-let find_products = (str) => {
-	return new Promise((resolve, reject) => {
-		db_search('products', 'product_name', str, products => {
-			console.log('2', products.length);
-			resolve(products);
-		});
-	});
-}
-let find_promotions = (str) => {
-	return new Promise((resolve, reject) => {
-		db_search('promotions', 'promotion_name', str, promotions => {
-			console.log('3', promotions.length);
-			resolve(promotions);
+		db_read('users', str, users => {
+			console.log('1', users.length);
+			resolve(users);
 		});
 	});
 }
 router.post('/search', (req, res, next) => {
-	let response={};
-
 	const findStr = req.body.searchString;
-	if (findStr && findStr.length > 1) {
-		/**
-		 * Figure out if findStr is invalid and queries produce errors
-		 */
-		 let p1 =find_suppliers('%'+ findStr.toLowerCase()+ '%');
-		 let p2 =find_products('%'+ findStr.toLowerCase()+ '%');
-		 let p3 =find_promotions('%'+ findStr.toLowerCase()+ '%');
-
-		 Promise.all([p1, p2, p3]).then(values => {
-			 console.log('v', values);
-			 response = { suppliers: values[0], products: values[1], promotions: values[2] }
-			res.send(response);
-		 });
-		 
+	if (findStr && findStr.length.trim() > 1) {
+		find_users(findStr)
+		.then(users => res.send(users))
+		.catch(e => res.send('No Results ound'));
 	} else {
 		res.send("String too short");
 	}
