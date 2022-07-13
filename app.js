b6fc02e5a46a89cf4e7c8b969857c0a13c85cc4b
mozn-lz <mozn.lozn2000@gmail.com>
2022-07-13 18:20:58 +0200

    .env

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  console.log('err time ', Date.now());
   // render the error page
   res.status(err.status || 500);
   res.render('error');
